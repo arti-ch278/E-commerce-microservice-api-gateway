3eed979f7713da9380ce89c3732f4d18bd13ab00
Arti <arti.chourey21@gmail.com>
2026-03-28 14:19:45 +0530

    feat(api-gateway): improve route-specific filter with custom header injection

--- src/main/java/com/artichourey/ecommerce/gateway/filter/CustomRouteFilter.java
+++ src/main/java/com/artichourey/ecommerce/gateway/filter/CustomRouteFilter.java
@@ -4,28 +4,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.Mono;
 @Component
 public class CustomRouteFilter extends AbstractGatewayFilterFactory<Object> {
-	
-	private final Logger log= LoggerFactory.getLogger(CustomRouteFilter.class);
 
-	public CustomRouteFilter()
-	{
-		super(Object.class);
-	}	
-	@Override
-	public GatewayFilter apply(Object config) {
-		// TODO Auto-generated method stub
-		return (exchange,chain)->{
-			log.info("Route filter: Before routing");
-			exchange.getRequest().mutate().header("X-Route-Header", "Added-By-Gateway")
-			.build();
-			return chain.filter(exchange).then(Mono.fromRunnable(()->{
-			log.info("Route Filter:After routing");
-		}));
-	};
-	}
+    private final Logger log = LoggerFactory.getLogger(CustomRouteFilter.class);
+
+    public CustomRouteFilter() {
+        super(Object.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+
+        return (exchange, chain) -> {
+
+            log.info("Route Filter: Before routing");
+
+            ServerHttpRequest modifiedRequest = exchange.getRequest()
+                    .mutate()
+                    .header("X-Route-Header", "Added-By-Gateway")
+                    .build();
+
+            return chain.filter(exchange.mutate().request(modifiedRequest).build())
+                    .then(Mono.fromRunnable(() ->
+                            log.info("Route Filter: After routing")));
+        };
+    }
 }
